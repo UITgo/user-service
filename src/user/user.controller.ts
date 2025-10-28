33f65f69fad4afb8d898259e34ae9d8759bf84e9
Nguyen Truong Ngoc Han <23520434@gm.uit.edu.vn>
2025-10-28 14:02:41 +0700

    minichanges: user patch

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller('users')
+@Controller('v1/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post()
